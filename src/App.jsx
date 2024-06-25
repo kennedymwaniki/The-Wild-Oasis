0f2aa9a733d4b51adb298c34026b695207d70234
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-06-25 05:54:10 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Users from "./pages/Users";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import AppLayout from "./ui/AppLayout";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,7 +30,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
