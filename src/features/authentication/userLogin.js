18d541baa48f3914a8d733743c70a42180941885
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-07-28 21:21:44 +0300

    login works

--- src/features/authentication/userLogin.js
+++ src/features/authentication/userLogin.js
@@ -1,16 +1,16 @@
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
-import { useMutation } from "@tanstack/react-query";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
+      queryClient.setQueryData(["user"], user.user);
       toast.success("login successful");
       navigate("/dashboard");
     },
